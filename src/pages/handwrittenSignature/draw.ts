252709b0327dce9e0d958dec031202ad285fe82d
黄笑 <huangxiao221@jd.com>
2021-03-04 01:08:15 +0800

    fix：改成react版本

--- src/pages/handwrittenSignature/draw.ts
+++ src/pages/handwrittenSignature/draw.ts
@@ -1,11 +1,23 @@
-import { ForwardRefRenderFunction, MutableRefObject } from 'react';
-
-const Draw = (canvas: any, degree: number) => {
+interface configType {
+  width?: number;
+  height?: number;
+  lineWidth?: number;
+}
+interface thisType {
+  context: any;
+}
+function Draw(
+  this: thisType,
+  canvas: any,
+  degree: number,
+  config?: configType,
+) {
+  console.log(this.context);
   if (!canvas) {
     console.log('warn');
     return;
   }
-  const isMobile = /phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone/i.test(
+  const isMobile = /phone|pad|iPhone|ios|iPad|Android|Mobile/i.test(
     navigator.userAgent,
   );
   interface pointObject {
@@ -17,31 +29,34 @@ const Draw = (canvas: any, degree: number) => {
     y: 0,
   };
   let pressed = false;
-  let real_canvas = canvas.current;
+  const real_canvas = canvas.current;
   let { width, height } = window.getComputedStyle(real_canvas, null);
-  const { left, top } = real_canvas.getBoundingClientRect();
+
   width = width.replace('px', '');
   height = height.replace('px', '');
-  let context = real_canvas.getContext('2d');
+  // let context = real_canvas.getContext('2d');
+  this.context = real_canvas.getContext('2d');
+  let context = this.context;
   // 根据设备像素比优化canvas绘图
   const devicePixelRatio = window.devicePixelRatio;
   if (devicePixelRatio) {
-    real_canvas.style.width = `${width}px`;
-    real_canvas.style.height = `${height}px`;
-    real_canvas.height = Number(height) * devicePixelRatio;
-    real_canvas.width = Number(width) * devicePixelRatio;
+    real_canvas.style.width = `${config?.width || width}px`;
+    real_canvas.style.height = `${config?.height || height}px`;
+    real_canvas.height = Number(config?.height || height) * devicePixelRatio;
+    real_canvas.width = Number(config?.width || width) * devicePixelRatio;
     context.scale(devicePixelRatio, devicePixelRatio);
   } else {
-    real_canvas.width = width;
-    real_canvas.height = height;
+    real_canvas.width = config?.width || width;
+    real_canvas.height = config?.height || height;
   }
-
-  context.lineWidth = 6;
+  const { left, top } = real_canvas.getBoundingClientRect();
+  context.lineWidth = config?.lineWidth || 6;
   context.strokeStyle = 'black';
   context.lineCap = 'round';
   context.lineJoin = 'round';
 
   const paint = (signal: number) => {
+    console.log(signal, 'signal');
     switch (signal) {
       case 1:
         context.beginPath();
@@ -55,7 +70,7 @@ const Draw = (canvas: any, degree: number) => {
   };
 
   const create = (signal: number) => (e: any) => {
-    console.log(signal, 'signal');
+    e.preventDefault();
     if (signal === 1) {
       pressed = true;
     }
@@ -72,6 +87,7 @@ const Draw = (canvas: any, degree: number) => {
     //   paint(signal);
     // }
   };
+
   const start = create(1);
   const move = create(2);
   const requestAnimationFrame = window.requestAnimationFrame;
@@ -101,6 +117,24 @@ const Draw = (canvas: any, degree: number) => {
       });
     });
   }
+}
+Draw.prototype = {
+  clear() {
+    let width;
+    let height;
+    switch (this.degree) {
+      case -90:
+      case 90:
+        width = this.height;
+        height = this.width;
+        break;
+      default:
+        width = this.width;
+        height = this.height;
+    }
+    // this.context.clearRect(0, 0, width, height);
+    console.log(this.context);
+  },
 };
 
 export default Draw;
--- src/pages/handwrittenSignature/index.tsx
+++ src/pages/handwrittenSignature/index.tsx
@@ -1,15 +1,118 @@
 import styles from './index.less';
-import { useEffect, useRef } from 'react';
-import Draw from './draw';
+import { useEffect, useRef, useState, useCallback } from 'react';
 
-export default function HandwrittenSignaturePage() {
-  const canvas = useRef(null);
-  useEffect(() => {
-    const canvas1 = Draw(canvas, 90);
+interface canvasPros {
+  width: number;
+  height: number;
+}
+type Coordinate = {
+  x: number;
+  y: number;
+};
+
+const HandwrittenSignaturePage = ({ width, height }: canvasPros) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [isPainting, setisPainting] = useState(false);
+  const [mousePosition, setMousePosition] = useState<Coordinate | undefined>(
+    undefined,
+  );
+  const startPaint = useCallback((event: MouseEvent) => {
+    const coordinates = getCoordinates(event);
+    console.log(coordinates, 'start');
+    if (coordinates) {
+      setisPainting(true);
+      setMousePosition(coordinates);
+    }
   }, []);
+
+  const paint = useCallback(
+    (event: MouseEvent) => {
+      console.log(isPainting, 'isPainting');
+      if (isPainting) {
+        const newMousePosition = getCoordinates(event);
+        console.log(mousePosition, newMousePosition, isPainting, 'move');
+        if (mousePosition && newMousePosition) {
+          darwLine(mousePosition, newMousePosition);
+          setMousePosition(newMousePosition);
+        }
+      }
+    },
+    [isPainting, mousePosition],
+  );
+
+  const exitPaint = useCallback(() => {
+    console.log('exit');
+    setisPainting(false);
+  }, []);
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+    const canvas = canvasRef.current;
+    canvas?.addEventListener('mousedown', startPaint);
+    return () => {
+      canvas?.removeEventListener('mousedown', startPaint);
+    };
+  }, [startPaint]);
+
+  useEffect(() => {
+    if (!canvasRef) return;
+    const canvas = canvasRef.current;
+    canvas?.addEventListener('mousemove', paint);
+    return () => {
+      canvas?.removeEventListener('mousemove', paint);
+    };
+  }, [paint]);
+
+  useEffect(() => {
+    if (!canvasRef) return;
+    const canvas = canvasRef.current;
+    canvas?.addEventListener('mouseup', exitPaint);
+    canvas?.addEventListener('mouseleave', exitPaint);
+    return () => {
+      canvas?.removeEventListener('mouseup', exitPaint);
+      canvas?.removeEventListener('mouseleave', exitPaint);
+    };
+  }, [exitPaint]);
+
+  const darwLine = (oldPostion: Coordinate, newPosition: Coordinate) => {
+    if (!canvasRef.current) return;
+    const canvas: HTMLCanvasElement = canvasRef.current;
+    const context = canvas.getContext('2d');
+    console.log('drawline1');
+    if (context) {
+      console.log('drawline2');
+      context.lineWidth = 6;
+      context.strokeStyle = 'black';
+      context.lineCap = 'round';
+      context.lineJoin = 'round';
+
+      context.beginPath();
+      context.moveTo(oldPostion.x, oldPostion.y);
+      context.lineTo(newPosition.x, newPosition.y);
+      context.closePath();
+      context.stroke();
+    }
+  };
+
+  const getCoordinates = (event: MouseEvent): Coordinate | undefined => {
+    if (!canvasRef.current) return;
+    const canvas: HTMLCanvasElement = canvasRef.current;
+    const { left, top } = canvas.getBoundingClientRect();
+    return {
+      x: event.clientX - left,
+      y: event.clientY - top,
+    };
+  };
+
   return (
     <div className={styles.signature}>
-      <canvas ref={canvas}></canvas>
+      <canvas ref={canvasRef} height={height} width={width}></canvas>
     </div>
   );
-}
+};
+
+// HandwrittenSignaturePage.defaultProps = {
+//   width: window.innerWidth,
+//   height: window.innerHeight,
+// };
+export default HandwrittenSignaturePage;
